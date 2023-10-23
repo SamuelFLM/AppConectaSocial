d48fda35a1020c4c3df06c3fa4f9468afec6bb3f
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-23 10:44:38 -0300

    add: readme

---
+++ AppSocialMedia/obj/Debug/net6.0-android/android/src/crc64d1afa2369b8b2181/MainApplication.java
@@ -0,0 +1,34 @@
+package crc64d1afa2369b8b2181;
+
+
+public class MainApplication
+	extends crc6488302ad6e9e4df1a.MauiApplication
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+	}
+
+	public MainApplication ()
+	{
+		mono.MonoPackageManager.setContext (this);
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+<img src="Imagens/logo.png" alt="logo_samuelflm">
+
+
+## Conecta Social: Seu Hub de Mídias Sociais
+
+Bem-vindo ao Conecta Social, o aplicativo que reúne todas as suas redes sociais favoritas em um só lugar.
+
+Navegue, interaja e mantenha-se conectado com amigos, família e o mundo ao seu redor, tudo a partir de um único aplicativo. Economize tempo, esforço e espaço no seu dispositivo com o Conecta Social!
+
+## Interface:
+
+<img src="Imagens/app.png" alt="logo_samuelflm">
+
+<img src="Imagens/menu.png" alt="logo_samuelflm">
+
+<img src="Imagens/youtube.png" alt="logo_samuelflm">
+
